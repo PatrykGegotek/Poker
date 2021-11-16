794aea68168ffd3b73fca274974d5ea4d63d5aeb
Patryk Gęgotek <patryk26g@gmail.com>
2021-11-16 23:13:35 +0100

    basic project structure

---
+++ poker-client/src/main/java/lab5/patryk/Main.java
@@ -0,0 +1,15 @@
+package lab5.patryk;
+
+import lab5.patryk.Model;
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Działa!" );
+        Model.model();
+    }
+}
---
+++ poker-model/src/main/java/lab5/patryk/Model.java
@@ -0,0 +1,7 @@
+package lab5.patryk;
+
+public class Model {
+    public static void model() {
+        System.out.println("lab5.patryk.Model działa!");
+    }
+}
